f858ae7e86af11f71ba1efd652687acaddcf4dd4
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2017-01-29 14:47:24 +0000

    Add graphical representation

---
+++ app/src/test/java/com/bowling/mauriziopietrantuono/bowling/model/RepresenterTest.java
@@ -0,0 +1,21 @@
+package com.bowling.mauriziopietrantuono.bowling.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RepresenterTest {
+
+    @Test
+    public void given_foo_when_bar_then_fobar() {
+
+        // GIVEN
+
+
+        //* WHEN
+
+
+        //* THEN
+
+    }
+}
