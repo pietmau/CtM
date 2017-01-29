97b498b1113f4c9afa931906949221b12750036b
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2017-01-29 21:47:08 +0000

    Use old scorer

--- app/src/main/java/com/bowling/mauriziopietrantuono/bowling/model/Scorer.java
+++ app/src/main/java/com/bowling/mauriziopietrantuono/bowling/model/Scorer.java
@@ -1,73 +1,84 @@
 package com.bowling.mauriziopietrantuono.bowling.model;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.bowling.mauriziopietrantuono.bowling.model.Constants.MAX_NUMBER_OF_FRAMES;
 import static com.bowling.mauriziopietrantuono.bowling.model.Constants.MAX_SCORE;
 
 public class Scorer {
-    private List<Frame> frames;
-    private List<Bonus> bonuses;
+    private List<Ball> balls = new ArrayList<>();
 
     public int score(BowlingMatch match) {
+        getBalls(match);
         int score = 0;
-        frames = match.getFrames();
-        bonuses = match.getBonuses();
+        for (int i = 0, frames = 0; i < balls.size() && frames < MAX_NUMBER_OF_FRAMES; i++, frames++) {
 
-        for (int framesIndex = 0; framesIndex < frames.size(); framesIndex++) {
-            if (frames.get(framesIndex).isAStrike()) {
+            if (isaStrike(i)) {
                 score += MAX_SCORE;
-                score += getNextTwoScores(framesIndex);
+                score += getNextTwoScores(i);
                 continue;
             }
-            if (frames.get(framesIndex).isASpare()) {
+
+            if (isASpare(i)) {
                 score += MAX_SCORE;
-                score += getNextScore(framesIndex);
-                continue;
+                i++;
+                score += getNextScore(i);
+
+            } else {
+                score += balls.get(i).getScore();
+                i++;
+                if (!isLastBall(i)) {
+                    score += balls.get(i).getScore();
+                }
             }
-            score += frames.get(framesIndex).getScore();
         }
         return score;
     }
 
-
-    private int getNextTwoScores(int framesIndex) {
-        int score = 0;
-        if (framesIndex > (frames.size() + bonuses.size()) - 1) {
-            return score;
+    private void getBalls(BowlingMatch match) {
+        balls.clear();
+        for (Frame frame : match.getFrames()) {
+            balls.add(frame.first);
+            if (frame.second != null) {
+                balls.add(frame.second);
+            }
         }
-        if (framesIndex < frames.size() - 2) {
-            score += getNextTwoFramesScores(framesIndex);
-            return score;
+        for (Bonus bonus : match.getBonuses()) {
+            balls.add(bonus.ball);
         }
-
-        if (framesIndex < frames.size() - 1) {
-            score += getNextScore(framesIndex);
-        }
-        return score;
     }
 
-    private int getNextTwoFramesScores(int framesIndex) {
-        int score = 0;
-        if (!frames.get(framesIndex + 1).isAStrike()) {
-            score += frames.get(framesIndex + 1).getScore();
-            return score;
-        }
-        score += MAX_SCORE;
-        framesIndex++;
-        score += getNextScore(framesIndex);
-        return score;
+    private boolean isLastBall(int i) {
+        return i >= balls.size();
     }
 
-    private int getNextScore(int framesIndex) {
-        if (framesIndex < frames.size() - 1) {
-            return frames.get(framesIndex + 1).first.getScore();
+    private int getNextScore(int i) {
+        if (i >= balls.size() - 1) {
+            return 0;
         }
-        if (framesIndex < frames.size() + bonuses.size() - 1) {
-            return bonuses.get((framesIndex + 1) - frames.size()).getScore();
+        return balls.get(i + 1).getScore();
+    }
+
+    private boolean isASpare(int i) {
+        if (i > balls.size() - 2) {
+            return false;
         }
-        return 0;
+        return (balls.get(i).getScore() + balls.get(i + 1).getScore()) == MAX_SCORE;
+    }
+
+    private boolean isaStrike(int index) {
+        Ball ball = balls.get(index);
+        return ball.getScore() == MAX_SCORE;
+    }
+
+    private int getNextTwoScores(int i) {
+        int nextTwoScores = 0;
+        for (int j = i + 1; j < balls.size() && j < i + 3; j++) {
+            nextTwoScores += balls.get(j).getScore();
+        }
+        return nextTwoScores;
     }
 
 }
